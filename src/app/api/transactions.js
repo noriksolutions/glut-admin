8acebe210be065416952f16d15d433621fffdace
Jason Sebring <mail@jasonsebring.com>
2015-11-26 14:50:27 -0800

    transaction update

--- src/app/api/transactions.js
+++ src/app/api/transactions.js
@@ -1,5 +1,13 @@
 module.exports = {
 	set: function(api) {
-		api.transactions = { };
+		api.transactions = {
+			list: function(options) {
+				return api._get('/transactions', options);
+			},
+			one: function(options) {
+				let id = options._id || options.id;
+				return api._get('/transactions/' + id);
+			}
+		};
 	}
 };
